7ca7c035d0311f6618fa1279e05feaca1c280c5e
jieyingong <gongjieying0312@gmail.com>
2025-06-30 18:10:59 -0400

    refactor(server): remove Sequelize sync and clean up .gitignore

--- server/app.js
+++ server/app.js
@@ -15,13 +15,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 try {
-  await sequelize.authenticate();
-  await sequelize.sync({ alter: { drop: false } });
+  await sequelize.authenticate(); // Test the connection to the PostgreSQL database
   console.log("Connection has been established successfully.");
 } catch (error) {
   console.error("Unable to connect to the database:", error);
 }
-
 
 
 app.get("/", (req, res) => {
