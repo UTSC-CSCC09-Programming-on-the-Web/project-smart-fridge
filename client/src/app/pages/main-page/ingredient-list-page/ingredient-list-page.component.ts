f7b5b75e75e3765b4a6e1410ae5247a427453a24
jieyingong <gongjieying0312@gmail.com>
2025-06-30 14:22:23 -0400

    feat(client): add mock ingredient list logic and sort by expire_date

--- client/src/app/pages/main-page/ingredient-list-page/ingredient-list-page.component.ts
+++ client/src/app/pages/main-page/ingredient-list-page/ingredient-list-page.component.ts
@@ -8,5 +8,23 @@ import { Ingredient } from '../../../models/ingredient.model';
     standalone: false
 })
 export class IngredientListPageComponent {
+  ingredients: Ingredient[] = [];
+  // this is a mock data, in real application, it should be fetched from the server
+  handleNewIngredient(newIngredient: Partial<Ingredient>) {
+    const newId = this.ingredients.length + 1;
 
+    const completeIngredient: Ingredient = {
+      ...newIngredient,
+      id: newId,
+      is_expired: false,
+      added_at: new Date().toISOString(),
+      fridge_id: 'mock-fridge-id',
+    } as Ingredient;
+
+    this.ingredients.unshift(completeIngredient);
+// Sort the ingredients by expire_date in ascending order
+// temporary solution, in real application, it should be sorted by the server
+      this.ingredients.sort(
+        (a, b) => new Date(a.expire_date).getTime() - new Date(b.expire_date).getTime());
+}
 } 
