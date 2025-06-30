a8162909e16f323a1080b982f859ac146bbbdb8f
jieyingong <gongjieying0312@gmail.com>
2025-06-30 02:03:33 -0400

    feat(client): add Ingredient interface in model with full fields design

---
+++ client/src/app/models/ingredient.model.ts
@@ -0,0 +1,15 @@
+// src/app/models/ingredient.model.ts
+
+export interface Ingredient {
+  id:  number;         // int
+  name: string;         
+  quantity: number;          
+  unit: string;                // will change to enum in the future
+  expire_date: string;         // ISO format date: '2025-07-01'
+  added_at: string;           // ISO format date: '2023-10-01T12:00:00Z'
+  type?: string;             // will change to enum in the future
+  is_expired: boolean;         
+  image_url?: string;  // URL to the image of the ingredient, if empty, use a default image
+  user_id: string;    // uuid（FK）
+  fridge_id: string;  // uuid（FK）
+}
