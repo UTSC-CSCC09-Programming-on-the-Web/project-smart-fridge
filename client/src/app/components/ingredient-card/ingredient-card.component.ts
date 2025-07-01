934818df4811ad4139002b7a2c283085f21d166e
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-01 16:07:15 -0400

    feat(ingredient): add delete confirmation dialog in IngredientCard using ConfirmDialogComponent
    - Manually tested and confirmed working as expected

--- client/src/app/components/ingredient-card/ingredient-card.component.ts
+++ client/src/app/components/ingredient-card/ingredient-card.component.ts
@@ -1,6 +1,8 @@
 // ingredient-card.component.ts
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Ingredient } from '../../models/ingredient.model';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-ingredient-card',
@@ -23,8 +25,25 @@ export class IngredientCardComponent {
     this.updateIngredient.emit(this.ingredient);
     console.log('Update ingredient:', this.ingredient);
   }
+
+  constructor(private dialog: MatDialog) {}
+
   onDeleteIngredient(): void {
-    this.deleteIngredient.emit(this.ingredient);
-    console.log('Delete ingredient:', this.ingredient);
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+    data: {
+      title: 'Delete Ingredient',
+      message: `Are you sure you want to delete ${this.ingredient.name}? This action cannot be undone.`,
+      confirmText: 'Delete',
+      cancelText: 'Cancel',
+    }
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) {
+      // If the user confirmed the deletion, emit the delete event
+      this.deleteIngredient.emit(this.ingredient);
+      console.log('Confirmed deletion of ingredient:', this.ingredient);
+    }
+  });
   }
 }
