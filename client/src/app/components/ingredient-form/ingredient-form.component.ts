b7cd207c171aaae0f902a49140cfbcf9dd853d51
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-01 17:32:21 -0400

    feat(client/ui): refactor IngredientFormComponent to support add/edit modes

--- client/src/app/components/ingredient-form/ingredient-form.component.ts
+++ client/src/app/components/ingredient-form/ingredient-form.component.ts
@@ -1,6 +1,12 @@
 // ingredient-form.component.ts
 
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  Input,
+  SimpleChanges,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Ingredient } from '../../models/ingredient.model';
 
@@ -13,7 +19,11 @@ import { Ingredient } from '../../models/ingredient.model';
 export class IngredientFormComponent {
   ingredientForm: FormGroup;
 
+  @Input() mode: 'add' | 'edit' = 'add';
+  @Input() ingredientToEdit?: Ingredient;
+
   @Output() submitIngredient = new EventEmitter<Partial<Ingredient>>();
+  @Output() cancelEdit = new EventEmitter<void>();
 
   constructor(private fb: FormBuilder) {
     this.ingredientForm = this.fb.group({
@@ -26,12 +36,49 @@ export class IngredientFormComponent {
     });
   }
 
+  // This method is called when the component receives new input properties
+  // It updates the form values if the mode is 'edit' and ingredientToEdit is provided
+  // This is useful for pre-filling the form with existing ingredient data when editing
+  ngOnChanges(changes: SimpleChanges): void {
+    if (
+      this.mode === 'edit' &&
+      changes['ingredientToEdit'] &&
+      this.ingredientToEdit
+    ) {
+      this.ingredientForm.patchValue({
+        name: this.ingredientToEdit.name,
+        quantity: this.ingredientToEdit.quantity,
+        unit: this.ingredientToEdit.unit,
+        expire_date: this.ingredientToEdit.expire_date,
+        type: this.ingredientToEdit.type,
+        image_url: this.ingredientToEdit.image_url || '',
+      });
+      console.log('Form updated for editing:', this.ingredientToEdit);
+    }
+  }
+
   ngOnInit(): void {}
 
-  postNewIngredient(): void {
+  postIngredient(): void {
     if (this.ingredientForm.valid) {
-      this.submitIngredient.emit(this.ingredientForm.value);
+      let output: Partial<Ingredient>;
+
+    if (this.mode === 'edit' && this.ingredientToEdit) {
+      // If in edit mode, merge the existing ingredient with the form values
+      output = {
+        ...this.ingredientToEdit,
+        ...this.ingredientForm.value
+      };
+    } else {
+      output = this.ingredientForm.value;
+    }
+
+
+      this.submitIngredient.emit(output);
+
+      if (this.mode === 'add'){
       this.ingredientForm.reset();
+      }
     } else {
       console.error('Form is invalid');
     }
