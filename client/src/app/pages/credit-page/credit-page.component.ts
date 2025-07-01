163467a6767ba6abfc9f6591e8b94cee2297b461
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-01 12:20:12 -0400

    feat(client): configure assets path and add default ingredient image & credit page
    Finalized the minimum functional frontend-backend loop without any errors.

---
+++ client/src/app/pages/credit-page/credit-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credit-page',
+  imports: [],
+  templateUrl: './credit-page.component.html',
+  styleUrl: './credit-page.component.scss'
+})
+export class CreditPageComponent {
+
+}
