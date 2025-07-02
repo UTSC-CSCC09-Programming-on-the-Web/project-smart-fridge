f1d644ccda54892e71bb6d05d373fd4466102abb
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-02 17:34:33 -0400

    refactor(server/middleware): ensure upload directory exists when saving image files

--- server/middlewares/image-upload-multer.js
+++ server/middlewares/image-upload-multer.js
@@ -8,7 +8,14 @@ const path = require('path');
 function createStorage(folder) {
   return multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, `../uploads/${folder}`));
+      const targetPath = path.join(__dirname, `../uploads/${folder}`);
+      // if the target path does not exist, create it
+      // for future, we move the images under each fridge's own uploads folder
+      if (!fs.existsSync(targetPath)) {
+        fs.mkdirSync(targetPath, { recursive: true });
+      }
+
+      cb(null, targetPath);
     },
     filename: function (req, file, cb) {
       const ext = path.extname(file.originalname);
