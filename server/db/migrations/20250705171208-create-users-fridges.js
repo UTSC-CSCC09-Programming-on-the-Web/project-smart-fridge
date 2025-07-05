344adb5a54e479ef054a3db19b9df430f607e10e
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-05 13:13:47 -0400

    chore(server/models): implement many-to-many relationship between users and fridges with UserFridge model and add db migrations

---
+++ server/db/migrations/20250705171208-create-users-fridges.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users_fridges', {
+      user_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      fridge_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'fridges',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+
+    await queryInterface.addConstraint('users_fridges', {
+      fields: ['user_id', 'fridge_id'],
+      type: 'unique',
+      name: 'unique_user_fridge'
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users_fridges');
+  },
+};
