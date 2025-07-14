dc6d196932903c4916950c1a25f5fd8c7e072e1a
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-14 00:33:19 -0400

    feat(user-auth): enforce authentication middleware on user, fridge and ingredient routes

--- server/routers/ingredients-router.js
+++ server/routers/ingredients-router.js
@@ -9,17 +9,18 @@ const {
 } = require("../controllers/ingredients-controller.js");
 const getImageUploadMiddleware = require("../middlewares/image-upload-multer.js");
 const fridgeAuthMiddle = require("../middlewares/fridge-author-middleware.js");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 const router = express.Router();
 
-router.get("/:fridge_id/ingredients", fridgeAuthMiddle, getIngredientsInfiniteScroll);
+router.get("/:fridge_id/ingredients", authMiddleware, fridgeAuthMiddle, getIngredientsInfiniteScroll);
 
 const ingredientImageUpload = getImageUploadMiddleware({
   folder: "ingredients",
 });
-router.post("/:fridge_id/ingredients", fridgeAuthMiddle, ingredientImageUpload, createIngredient);
+router.post("/:fridge_id/ingredients", authMiddleware, fridgeAuthMiddle, ingredientImageUpload, createIngredient);
 
-router.put("/:fridge_id/ingredients/:id", fridgeAuthMiddle, updateIngredient);
-router.delete("/:fridge_id/ingredients/:id", fridgeAuthMiddle, deleteIngredient);
+router.put("/:fridge_id/ingredients/:id", authMiddleware, fridgeAuthMiddle, updateIngredient);
+router.delete("/:fridge_id/ingredients/:id", authMiddleware, fridgeAuthMiddle, deleteIngredient);
 
 module.exports = router;
