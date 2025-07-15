4906bfd57e3316927b73d2b050773f1cd6334ee2
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-15 02:00:05 -0400

    refactor(server/recipe): refactor recipe generation logic to handle job queue and response

--- server/controllers/recipe-controller.js
+++ server/controllers/recipe-controller.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { getIngredientsForRecipe } = require("../services/ingredient-service.js");
+const {llmQueue, addLlmJob, LLM_JOB_TYPES} = require("../queues/llm-queue.js");
 
 // POST /api/recipes/generate
 const postGenerateRecipe = async (req, res) => {
@@ -8,14 +9,31 @@ const postGenerateRecipe = async (req, res) => {
   try{
     const fridgeId = req.fridgeId || req.body.fridgeId; // for single fridge right now
     const ingredients = await getIngredientsForRecipe(fridgeId);
-    res.status(200).json({
+    if (!ingredients || ingredients.length === 0) {
+      return res.status(400).json({ error: 'No ingredients available for recipe generation' });
+    }
+    const jobType = LLM_JOB_TYPES.RecipeGenerate;
+    const jobData = {
       ingredients: ingredients.map(ingredient => ({
         id: ingredient.id,
         name: ingredient.name,
         quantity: ingredient.quantity,
         expire_date: ingredient.expire_date,
       })),
-      recipe:  "Sample Recipe back from controller"});  
+      fridge_id: fridgeId,
+      user_id: req.user?.id, 
+    };
+    const { job, llmTaskRecord } = await addLlmJob(jobType, jobData);
+    console.log(`Job ${job.id} added to the queue for recipe generation`);
+    console.log(`LlmTask record created with ID: ${llmTaskRecord.id}`);
+    res.status(202).json({
+      ingredients: ingredients.map(ingredient => ({
+        id: ingredient.id,
+        name: ingredient.name,
+        quantity: ingredient.quantity,
+        expire_date: ingredient.expire_date,
+      })),
+      recipe:  "Accepted! Generating recipe..."});  
   } catch (error) {
       console.error('Error generating recipe:', error);
       res.status(500).json({ error: 'Failed to generate recipe' });
