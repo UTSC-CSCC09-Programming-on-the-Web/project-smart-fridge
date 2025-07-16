b0224cb07061fd6ed224befe09d000b6a7c4a0ed
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-16 00:15:48 -0400

    feat(multi-ingreidents): implement multi-image upload functionality with service integration

--- client/src/app/pages/main-page/ingredient-input-page/ingredient-input-page.component.ts
+++ client/src/app/pages/main-page/ingredient-input-page/ingredient-input-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AddMultiIngredientsService } from '../../../services/add-multi-ingredients.service';
 
 @Component({
   selector: 'app-ingredient-input-page',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './ingredient-input-page.component.scss',
   standalone: false,
 })
-export class IngredientInputPageComponent {}
+export class IngredientInputPageComponent {
+
+  constructor(private addMultiIngredientsService: AddMultiIngredientsService) {}
+
+  handleMultiImagesUploaded(images: File[]): void {
+    console.log('Ingredient Input Page: Images uploaded:', images);
+    const formData = new FormData();
+    images.forEach((image, index) => {
+      formData.append(`image${index}`, image);
+    });
+    this.addMultiIngredientsService.postImagesToServer(formData).subscribe({
+      next: () => console.log('Images uploaded successfully'),
+      error: (err) => console.error('Error uploading images:', err)
+    });
+  }
+}
