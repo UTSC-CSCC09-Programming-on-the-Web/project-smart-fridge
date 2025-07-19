6be174da7ed77579f4f3143cca148110ed9146f0
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-19 00:03:30 -0400

    chore: add log and try debug

--- server/redis/redis-socket.js
+++ server/redis/redis-socket.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 
 const redisUrl = process.env.REDIS_URL;
 
-const pubClient = createClient(redisUrl);
+const pubClient = createClient({ url: redisUrl });
 const subClient = pubClient.duplicate();
 
 async function connectSocketRedis() {
   console.log("Connecting to Redis at", redisUrl);
+  console.log("debug pubClient", pubClient.options.host, pubClient.options.port);
   await pubClient.connect();
   await subClient.connect();
 }
