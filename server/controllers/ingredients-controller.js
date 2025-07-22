025d59442008a1327d39025f2aea47de402d86c2
Jieying Gong <jieying.gong@mail.utoronto.ca>
2025-07-22 01:32:52 -0400

    refactor(server/ingredients): integrate GCS for image deletion in deleteIngredient function，finishing whole integration with gcs for image handling

--- server/controllers/ingredients-controller.js
+++ server/controllers/ingredients-controller.js
@@ -8,6 +8,7 @@ const fs = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 const getImageUrl = require("../utils/image-url.js");
+const { deleteFileFromGCS } = require("../services/gcs-storage-service.js");
 
 // for infintie scroll pagination, we use expire date and id as cursors
 // GET /api/fridges/:fridgeId/ingredients?limit=10&expireDateCursor=2025-07-01&idCursor=123
@@ -152,15 +153,10 @@ const deleteIngredient = async (req, res) => {
     }
 
     if (ingredient.image_url) {
-      const imagePath = path.join(
-        __dirname,
-        "..",
-        "uploads/",
-        ingredient.image_url
-      );
+      const imagePath = ingredient.image_url;
       console.log("Deleting image at:", imagePath);
       try {
-        await fs.promises.unlink(imagePath);
+        await deleteFileFromGCS(imagePath);
         console.log(`Image deleted: ${imagePath}`);
       } catch (err) {
         console.warn(`Warning: Failed to delete image: ${imagePath}`, err);
